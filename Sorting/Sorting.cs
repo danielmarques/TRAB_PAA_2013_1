7395acf32f1379a56654ca3f43a8d68017d926d1
Daniel da Rosa Marques <dmarques@inf.puc-rio.br>
2013-06-07 00:42:01 -0300

    Adicionados metodos relativos ao heap min

--- Sorting/Sorting.cs
+++ Sorting/Sorting.cs
@@ -13,6 +13,7 @@ namespace Sorting
     public class Sorting<TValue>
     {
         #region Heap
+
         /// <summary>
         /// Ordena o vetor de entrada usando o heap sort.
         /// </summary>
@@ -38,25 +39,63 @@ namespace Sorting
         }
 
         /// <summary>
+        /// Extrai e retorna o elemento de menor prioridade do Heap Min
+        /// </summary>
+        /// <param name="heapMin">Lista de elementos com suas prioridades</param>
+        public static Tuple<int, TValue> HeapExtractMin(ref List<Tuple<int, TValue>> heapMin)
+        {
+            //Salva o primeiro elemento do heap (mínimo)
+            var root = heapMin[0];
+
+            //Troca o primeiro elemento com o último
+            SwapElements(ref heapMin, 0, heapMin.Count-1);
+
+            //Diminui o tamanho do heap deletando o útimo elemento
+            heapMin.RemoveAt(heapMin.Count-1);
+
+            //Restaura a propriedade do heap
+            HeapfyDownMin(ref heapMin, heapMin.Count, 0);
+
+            return root;
+        }
+
+        /// <summary>
         /// Recebe uma lista de prioridades reordena a mesma na forma de Heap Max in place em O(n).
         /// </summary>
         /// <param name="priorityQueue">Lista de prioridades</param>
         private static void HeapfyMax(ref List<Tuple<int, TValue>> priorityQueue)
         {
             //Recupera o tamanho da lista
-            int heapLenght = priorityQueue.Count;
+            int heapSize = priorityQueue.Count;
 
-            //Executa o heapfyDown para cada elemento a partir da metade da lista para o primeiro
+            //Executa o heapfyDown para cada elemento a partir da metade da lista até o primeiro
             //Cria o heap em O(n)
-            for (int i = (int)Math.Ceiling(heapLenght / 2.0); i >= 0; i--)
+            for (int i = (int)Math.Ceiling(heapSize / 2.0); i >= 0; i--)
             {
-                HeapfyDownMax(ref priorityQueue, heapLenght, i);
+                HeapfyDownMax(ref priorityQueue, heapSize, i);
+            }
+        }
+
+        /// <summary>
+        /// Recebe uma lista de prioridades reordena a mesma na forma de Heap Min in place em O(n).
+        /// </summary>
+        /// <param name="priorityQueue">Lista de prioridades</param>
+        private static void HeapfyMin(ref List<Tuple<int, TValue>> priorityQueue)
+        {
+            //Recupera o tamanho da lista
+            int heapSize = priorityQueue.Count;
+
+            //Executa o heapfyDown para cada elemento a partir da metade da lista até o primeiro
+            //Cria o heap em O(n)
+            for (int i = (int)Math.Ceiling(heapSize / 2.0); i >= 0; i--)
+            {
+                HeapfyDownMin(ref priorityQueue, heapSize, i);
             }
         }
 
         /// <summary>
         /// Compara a prioridade de um elemento do Heap Max com seus filhos e troca posições quando necessário.
-        /// Faz isso recusrsivamente até que o elemento esteja na posição correta no Heap Min, ou seja, sua prioridade é menor do que a dos filhos.
+        /// Faz isso recusrsivamente até que o elemento esteja na posição correta no Heap Max, ou seja, sua prioridade é maior do que a dos filhos.
         /// </summary>
         /// <param name="priorityQueue">Lista de prioridades de onde o elemento faz parte</param>
         /// <param name="heapSize">Tamanho do heap</param>
@@ -64,7 +103,6 @@ namespace Sorting
         /// <returns></returns>
         private static void HeapfyDownMax(ref List<Tuple<int, TValue>> priorityQueue, int heapSize, int elementPosition)
         {
-
             //Variaveis auxiliares para representar os filhos
             int leftChild = elementPosition * 2 + 1;
             int rightChild = elementPosition * 2 + 2;
@@ -89,7 +127,80 @@ namespace Sorting
                 //Mantem a porpriedade do heap no nívelm ais baixo
                 HeapfyDownMax(ref priorityQueue, heapSize, bigger);
             }
+        }
 
+        /// <summary>
+        /// Compara a prioridade de um elemento do Heap Min com seus filhos e troca posições quando necessário.
+        /// Faz isso recusrsivamente até que o elemento esteja na posição correta no Heap Min, ou seja, sua prioridade é maior do que a dos filhos.
+        /// </summary>
+        /// <param name="priorityQueue">Lista de prioridades de onde o elemento faz parte</param>
+        /// <param name="heapSize">Tamanho do heap</param>
+        /// <param name="elementPosition">Posição do elemento na lista de prioridades</param>
+        private static void HeapfyDownMin(ref List<Tuple<int, TValue>> priorityQueue, int heapSize, int elementPosition)
+        {
+            //Variaveis auxiliares para representar os filhos
+            int leftChild = elementPosition * 2 + 1;
+            int rightChild = elementPosition * 2 + 2;
+            //Variavel que será usada para guardar o menor entre elemento atual, filho da direita e filho da esquerda
+            int smaller = 0;
+
+            //Verifica se o filho da esquerda é válido e se é menor do que o elemento atual
+            if (leftChild < heapSize && priorityQueue[leftChild].Item1 < priorityQueue[elementPosition].Item1)
+                smaller = leftChild;
+            else
+                smaller = elementPosition;
+
+            //Verifica se o filho da direita é válido e se é maior do que o menor calculado no passo anterior
+            if (rightChild < heapSize && priorityQueue[rightChild].Item1 < priorityQueue[smaller].Item1)
+                smaller = rightChild;
+
+            //Se o elemento não é menor do que o menor dos filhos
+            if (smaller != elementPosition)
+            {
+                //Troca os elementos para manter a propriedade do heap entre eles
+                SwapElements(ref priorityQueue, smaller, elementPosition);
+                //Mantem a porpriedade do heap no nível mais baixo
+                HeapfyDownMin(ref priorityQueue, heapSize, smaller);
+            }
+        }
+
+        /// <summary>
+        /// Mantem a propriedade do Heap para cima
+        /// </summary>
+        /// <param name="priorityQueue">Lista de prioridades de onde o elemento faz parte</param>
+        /// <param name="heapSize">Tamanho do heap</param>
+        /// <param name="elementPosition">Posição do elemento na lista de prioridades</param>
+        private static void HeapfyUpMin(ref List<Tuple<int, TValue>> priorityQueue, int heapSize, int elementPosition)
+        {
+            int fatherPosition = 0;
+
+            //Verifica se a posição do elemento é par ou impar e calcula a posição do pai de acordo
+            //Verifica o resto da divisão por 2
+            if (elementPosition % 2 == 0)
+                //É par
+                fatherPosition = (elementPosition - 2) / 2;
+            else
+                //É impar
+                fatherPosition = (elementPosition - 1) / 2;
+
+            //Verifica se a prioridade do pai é maior do que a do elemento
+            if (priorityQueue[fatherPosition].Item1 > priorityQueue[elementPosition].Item1)
+            {
+                //Se for então troca os elemento para restaurar a propriedade do heap
+                SwapElements(ref priorityQueue, elementPosition, fatherPosition);
+                //Mantem a propriedade do heap no nível acima
+                HeapfyUpMin(ref priorityQueue, heapSize, fatherPosition);
+            }
+        }
+
+        /// <summary>
+        /// Altera o valor da prioridade de um elemento do heap.
+        /// </summary>
+        /// <param name="priorityQueue"></param>
+        /// <param name="newKeyValue"></param>
+        /// <param name="elementPosition"></param>
+        private static void HeapChangeKey(ref List<Tuple<int, TValue>> priorityQueue, int newKeyValue, int elementPosition)
+        {
 
         }
 
@@ -102,11 +213,9 @@ namespace Sorting
         /// <returns></returns>
         private static void SwapElements(ref List<Tuple<int, TValue>> priorityQueue, int elementPosition1, int elementPosition2)
         {
-
             var tempElement = priorityQueue[elementPosition1];
             priorityQueue[elementPosition1] = priorityQueue[elementPosition2];
             priorityQueue[elementPosition2] = tempElement;
-
         }
 
         #endregion
